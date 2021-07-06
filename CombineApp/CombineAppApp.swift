dec3d7fedd252d45fb37dba754befc9039c762ac
Thabang Thango <thango0411@gmail.com>
2021-07-06 20:09:23 +0200

    Initial commit

---
+++ CombineApp/CombineAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CombineAppApp.swift
+//  CombineApp
+//
+//  Created by Thabang Thango on 2021/07/06.
+//
+
+import SwiftUI
+
+@main
+struct CombineAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
